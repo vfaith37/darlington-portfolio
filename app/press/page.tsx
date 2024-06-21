ba30d1322a6e926957f7785ae1f1e1256c2ffda4
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-06-21 21:17:25 +0100

    basically reinstalled the app

---
+++ app/press/page.tsx
@@ -0,0 +1,16 @@
+// pages/press.tsx
+
+import Header from "../_components/Header";
+
+const Press = () => {
+  return (
+    <>
+      <Header />
+      <main className="container mx-auto p-4">
+        <h1>Press</h1>
+      </main>
+    </>
+  );
+};
+
+export default Press;
